8581ecdefab80eb3715d8754aa8892c4348ec7af
OmarElati <5796@holbertonstudents.com>
2023-08-18 23:48:31 +0200

    Lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -11,15 +11,14 @@ def join(T, R):
 
 
 def canUnlockAll(boxes):
-    index = 0
-    total_boxes = list(set(boxes[0]) | {0})
-    added = True
-    while added:
-        added = False
-        for j in join(boxes, total_boxes[index:]):
-            if j not in total_boxes:
-                total_boxes.append(j)
-                added = True
-        index += 1
+    total_boxes = set([0])
+    queue = [0]
+
+    while queue:
+        current_box = queue.pop(0)
+        for key in boxes[current_box]:
+            if key not in total_boxes:
+                total_boxes.add(key)
+                queue.append(key)
 
     return len(total_boxes) == len(boxes)
