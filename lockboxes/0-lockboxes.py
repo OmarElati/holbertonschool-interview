4a1111c12ffac7e14d66dcf90c447cffc8cd4df4
OmarElati <5796@holbertonstudents.com>
2023-08-18 23:38:19 +0200

    Lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
 """method that determines if all the boxes can be opened"""
+def join(T, R):
+    res = []
+    for e in R:
+        res += T[e]
+    return res
+
 def canUnlockAll(boxes):
-    total_boxes = len(boxes)
-    unlocked_boxes = set([0])
-    queue = [0]
+    index = 0
+    total_boxes = list(set(boxes[0]) | {0})
+    added = True
+    while added:
+        added = False
+        for j in join(boxes, total_boxes[index:]):
+            if j not in total_boxes:
+                total_boxes.append(j)
+                index = 1
+                added = True
 
-    while queue:
-        current_box = queue.pop(0)
-        for key in boxes[current_box]:
-            if key not in unlocked_boxes:
-                unlocked_boxes.add(key)
-                queue.append(key)
-
-    return len(unlocked_boxes) == total_boxes
+    return len(total_boxes) == len(boxes)
