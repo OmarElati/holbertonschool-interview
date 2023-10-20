4861a88e037fed695d88977105138f1a41240367
OmarElati <5796@holbertonstudents.com>
2023-10-20 20:55:20 +0200

    UTF-8 Validation

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -9,7 +9,7 @@ def validUTF8(data):
     """
     Determines if a given data set
     represents a valid UTF-8 encoding.
-    
+
     Args:
         data: A list of integers representing
         the bytes of a UTF-8 encoded string.
@@ -18,22 +18,20 @@ def validUTF8(data):
         True if data is a valid UTF-8 encoding,
         else return False.
     """
-
-    num_bytes = 0
     for byte in data:
         if num_bytes == 0:
-            if byte >> 7 == 0:
+            if byte >> 7 == 0b0:
                 num_bytes = 0
-            elif byte >> 5 == 6:
+            elif byte >> 5 == 0b110:
                 num_bytes = 1
-            elif byte >> 4 == 14:
+            elif byte >> 4 == 0b1110:
                 num_bytes = 2
-            elif byte >> 3 == 30:
+            elif byte >> 3 == 0b11110:
                 num_bytes = 3
             else:
                 return False
         else:
-            if byte >> 6 == 2:
+            if byte >> 6 == 0b10:
                 num_bytes -= 1
             else:
                 return False
