70aca99e3657f5f7407839f5d81144bb3871ed84
OmarElati <5796@holbertonstudents.com>
2024-05-10 20:12:05 +0200

    Heap sort

--- heap_sort/sort.h
+++ heap_sort/sort.h
@@ -7,9 +7,5 @@
 void print_array(const int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void heapify(int *array, size_t s, size_t root, size_t size);
-// Test Part
-void quick_sort(int *array, size_t size);
-void quicksort_recursive(int *array, int low, int high, size_t size);
-int partition(int *array, int low, int high);
 
 #endif /* SORT_H */
