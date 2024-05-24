0d313718d4ef88ba980cd09c5ca11a985598d313
OmarElati <5796@holbertonstudents.com>
2024-05-24 20:13:30 +0200

    Implement recursive function to count words in Reddit post titles

--- count_it/0-count.py
+++ count_it/0-count.py
@@ -1,53 +1,36 @@
 #!/usr/bin/python3
-"""
-Count it!
-"""
-from requests import request
+import requests
 
+def count_words(subreddit, word_list, after=None, counts=None):
+    if counts is None:
+        counts = {}
 
-def generate_dicts(word_list):
-    """
-    generate_dicts functions
-    """
-    count = {k: 0 for k in word_list}
-    dup = {}
-    for k in word_list:
-        if k not in dup:
-            dup[k] = 0
-        dup[k] += 1
-    return (count, dup)
+    if after is None:
+        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    else:
+        url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit, after)
+        
+    headers = {"User-Agent": "Counting Bot"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
+    if response.status_code != 200:
+        print("Invalid subreddit or no posts found.")
+        return
 
-def count_words(subreddit, word_list, after="", count={}, dup={}, init=0):
-    """
-    count_words function
-    """
-    if not init:
-        count, dup = generate_dicts(word_list)
+    data = response.json()
+    posts = data['data']['children']
+    
+    for post in posts:
+        title = post['data']['title'].lower()
+        for word in word_list:
+            word = word.lower()
+            if word in title:
+                counts[word] = counts.get(word, 0) + title.count(word)
 
-    url = "https://api.reddit.com/r/{}/hot?after={}".format(subreddit, after)
-    headers = {"User-Agent": "Python3"}
-    response = request("GET", url, headers=headers).json()
-    try:
-        data = response.get('data')
-        top = data.get('children')
-        _after = data.get('after')
-
-        for item in top:
-            data = item.get('data')['title']
-            for word in count:
-                amount = data.lower().split(' ').count(word.lower())
-                count[word] += amount
-
-        if _after:
-            count_words(subreddit, word_list, _after, count, dup, 1)
-        else:
-            sort_abc = sorted(count.items(), key=lambda tup: tup[::-1])
-            desc = sorted(sort_abc, key=lambda tup: tup[1], reverse=True)
-
-            for name, cnt in desc:
-                cnt *= dup[name]
-                if cnt:
-                    print('{}: {}'.format(name.lower(), cnt))
-    except Exception:
-        return None
+    next_page = data['data']['after']
+    if next_page is not None:
+        count_words(subreddit, word_list, next_page, counts)
+    else:
+        sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+        for word, count in sorted_counts:
+            print("{}: {}".format(word, count))
