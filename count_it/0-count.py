457eacf742059171f9de9bd1cf451fd1324c87b0
OmarElati <5796@holbertonstudents.com>
2024-05-24 20:21:39 +0200

    Implement recursive function to count words in Reddit post titles

--- count_it/0-count.py
+++ count_it/0-count.py
@@ -1,36 +1,48 @@
 #!/usr/bin/python3
+"""Count It"""
 import requests
 
-def count_words(subreddit, word_list, after=None, counts=None):
-    if counts is None:
-        counts = {}
+
+def count_words(subreddit, word_list, hot_list_titles=[], after='null'):
+    """ returns a list containing the titles
+    of all hot articles for a given subreddit.
+    """
+    base_url = 'https://www.reddit.com/r/'
+    url = base_url + subreddit + "/hot.json"
+    credentials = {'User-Agent': "Mozilla/5.0 (X11; CrOS x86_64 10066.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36"}
+    parameters = {"limit": "100", "after": after}
+    response = requests.get(url,
+                            headers=credentials,
+                            params=parameters,
+                            allow_redirects=False)
+    if response.status_code != 200:
+        return None
+
+    hot_list_of_dicts = response.json().get("data").get("children")
+    after = response.json().get("data").get("after")
+    """""to print the after string, which acts as a "pointer"
+    to the next response uncomment the following line"""
+    # print(after)
+    hot_list_titles.extend([reddit.get("data").get("title") for
+                            reddit in hot_list_of_dicts])
+    """to print the length of the hot_list_titles uncomment
+    the following line"""
+    # print(len(hot_list_titles))
 
     if after is None:
-        url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    else:
-        url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit, after)
-        
-    headers = {"User-Agent": "Counting Bot"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code != 200:
-        print("Invalid subreddit or no posts found.")
-        return
-
-    data = response.json()
-    posts = data['data']['children']
-    
-    for post in posts:
-        title = post['data']['title'].lower()
+        to_print_dict = {x: 0 for x in word_list}
         for word in word_list:
-            word = word.lower()
-            if word in title:
-                counts[word] = counts.get(word, 0) + title.count(word)
+            count = 0
+            for title in hot_list_titles:
+                split_title = title.split()
+                new_split = [element.lower() for element in split_title]
+                count = count + new_split.count(word.lower())
+            if count != 0:
+                to_print_dict[word] = to_print_dict[word] + count
 
-    next_page = data['data']['after']
-    if next_page is not None:
-        count_words(subreddit, word_list, next_page, counts)
+        for elem in sorted(to_print_dict.items(), key=lambda x: (-x[1], x[0])):
+            if elem[1] != 0:
+                print("{}: {}".format(elem[0], elem[1]))
     else:
-        sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
-        for word, count in sorted_counts:
-            print("{}: {}".format(word, count))
+        return count_words(subreddit, word_list,
+                           hot_list_titles, after)
