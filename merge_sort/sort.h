26c4b6c0228c2310a61a8fbee0b0be433853a307
OmarElati <5796@holbertonstudents.com>
2024-07-05 20:27:05 +0200

    Implement merge sort algorithm with detailed function documentation

---
+++ merge_sort/sort.h
@@ -0,0 +1,14 @@
+#ifndef _SORT_H_
+#define _SORT_H_
+
+/* INCLUDED LIBRARIES */
+#include <stdlib.h>
+#include <stdio.h>
+
+/* FUNCTION PROTOTYPES */
+void print_array(const int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void merge(int *array, int *temp, size_t left, size_t mid, size_t right);
+void merge_sort_recursive(int *array, int *temp, size_t left, size_t right);
+
+#endif /* _SORT_H_ */
