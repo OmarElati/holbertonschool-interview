ce6c6eb2797a10b2852c7221f7e1110db2339388
OmarElati <5796@holbertonstudents.com>
2024-07-05 20:38:02 +0200

    Add header file with function prototypes and include guards

--- merge_sort/sort.h
+++ merge_sort/sort.h
@@ -8,7 +8,6 @@
 /* FUNCTION PROTOTYPES */
 void print_array(const int *array, size_t size);
 void merge_sort(int *array, size_t size);
-void merge(int *array, int *temp, size_t left, size_t mid, size_t right);
-void merge_sort_recursive(int *array, int *temp, size_t left, size_t right);
+void merge_sort_helper(int *array, size_t size, int *tmp);
 
 #endif /* _SORT_H_ */
