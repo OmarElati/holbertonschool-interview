679cadbfcec1bae996757620aa1c3d033dac2036
OmarElati <5796@holbertonstudents.com>
2024-07-12 18:21:56 +0200

    Change comes from within

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
-'''Making Change module'''
+""" 0. Change comes from within """
 
 
 def makeChange(coins, total):
-    '''Function that makes change for a given total'''
+    """
+    Given a pile of coins of different values, determine the fewest number
+    of coins needed to meet a given amount total.
+    """
     if total < 1:
         return 0
-    change = 0
-    coins.sort(reverse=True)
-    for coin in coins:
-        temp_change = int(total / coin)
-        total -= (temp_change * coin)
-        change += temp_change
-        if total == 0:
-            return change
-    if total != 0:
-        return -1
+
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                dp[i] = min(dp[i], dp[i - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
