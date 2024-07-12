ecbcd7fd51a973fbcc90518324e79cd4f6549fa8
OmarElati <5796@holbertonstudents.com>
2024-07-12 17:04:20 +0200

    Change comes from within

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -3,7 +3,7 @@
 
 
 def makeChange(coins, total):
-    """ 
+    """
     Given a pile of coins of different values, determine the fewest number
     of coins needed to meet a given amount total.
     """
