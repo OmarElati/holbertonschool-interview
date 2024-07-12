852a11b29d04fe070be95ddad5ab1ca8117940c5
OmarElati <5796@holbertonstudents.com>
2024-07-12 17:18:03 +0200

    Change comes from within

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -7,17 +7,14 @@ def makeChange(coins, total):
     Given a pile of coins of different values, determine the fewest number
     of coins needed to meet a given amount total.
     """
-    if total < 0:
+    if total <= 0:
         return 0
-    coins.sort()
-    coins.reverse()
-    fewest = 0
+    
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+
     for coin in coins:
-        if total <= 0:
-            break
-        buff = total // coin
-        fewest += buff
-        total -= (buff * coin)
-    if total != 0:
-        return -1
-    return fewest
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+
+    return dp[total] if dp[total] != float('inf') else -1
