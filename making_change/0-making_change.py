d438428f55d55be2ac7300616a1d28bda4da19db
OmarElati <5796@holbertonstudents.com>
2024-07-12 17:47:37 +0200

    Change comes from within

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -1,24 +1,38 @@
 #!/usr/bin/python3
-""" Change comes from within """
+"""
+Defines function that determines the fewest number of coins to make change
+"""
 
 
-def makeChange(coins: list, total: int) -> int:
+def makeChange(coins, total):
     """
-    Given a pile of coins of different values, determine the fewest number
-    of coins needed to meet a given amount total.
+    Determines the fewest number of coins needed to meet a given total
+
+    parameters:
+        coins [list or positive ints]:
+            the values of the coins in your possession
+            you can assume you have an infinite number of coins of all values
+        total [int]:
+            total amount of change to make
+            if total is 0 or less, return 0
+
+    returns:
+        the fewest number of coins to make the change
+        or -1 if the total change cannot be made with the given coins
     """
     if total <= 0:
         return 0
-
-    inf = float('inf')
-    cache = [0] + [inf] * total
-
-    for i in range(len(cache)):
+    if len(coins) is 0:
+        return -1
+    coins = sorted(coins)
+    dynamic = [float('inf')] * (total + 1)
+    dynamic[0] = 0
+    for i in range(total + 1):
         for coin in coins:
-            if coin <= i:
-                cache[coin] = min(cache[i - coin], cache[coin])
-
-    if cache[total] != inf:
-        return cache[total]
-
-    return -1
+            if coin > i:
+                break
+            if dynamic[i - coin] != -1:
+                dynamic[i] = min(dynamic[i - coin] + 1, dynamic[i])
+    if dynamic[total] == float('inf'):
+        return -1
+    return dynamic[total]
