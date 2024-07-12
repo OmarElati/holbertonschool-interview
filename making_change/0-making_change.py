c2bb571b17024d418e463323b953fb9def8dbebf
OmarElati <5796@holbertonstudents.com>
2024-07-12 17:57:10 +0200

    Change comes from within

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -1,43 +1,38 @@
 #!/usr/bin/python3
 """
-0x19. Making Change, task 0. Change comes from within
+Defines function that determines the fewest number of coins to make change
 """
 
 
 def makeChange(coins, total):
     """
-    Given a set of coin denominations, determines the fewest coins needed to
-    meet a given total value.
+    Determines the fewest number of coins needed to meet a given total
 
-    Args:
-        coins (list of (int)s): list of coin denominations available
-        total (int): target total value
+    parameters:
+        coins [list or positive ints]:
+            the values of the coins in your possession
+            you can assume you have an infinite number of coins of all values
+        total [int]:
+            total amount of change to make
+            if total is 0 or less, return 0
 
-    Return:
-        total amount of coins needed to meet `total` value
-
+    returns:
+        the fewest number of coins to make the change
+        or -1 if the total change cannot be made with the given coins
     """
-    if type(total) is not int or type(coins) is not list or not all(
-            [type(coin) is int for coin in coins]):
-        print("Invalid args")
-        return 0
-
     if total <= 0:
         return 0
-
-    coins.sort(reverse=True)
-
-    total_coins = 0
-    remainder = total
-    for denom in coins:
-        if denom <= remainder:
-            curr = remainder // denom
-            total_coins += curr
-            remainder -= curr * denom
-        if remainder == 0:
-            break
-
-    if total_coins == 0 or remainder != 0:
+    if len(coins) is 0:
         return -1
-
-    return total_coins
+    coins = sorted(coins)
+    dynamic = [float('inf')] * (total + 1)
+    dynamic[0] = 0
+    for i in range(total + 1):
+        for coin in coins:
+            if coin > i:
+                break
+            if dynamic[i - coin] != -1:
+                dynamic[i] = min(dynamic[i - coin] + 1, dynamic[i])
+    if dynamic[total] == float('inf'):
+        return -1
+    return dynamic[total]
