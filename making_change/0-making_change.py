e1c3ef96d256096e91ba9ad5507591a57660fd9a
OmarElati <5796@holbertonstudents.com>
2024-07-12 18:18:52 +0200

    Change comes from within

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -9,10 +9,10 @@ def makeChange(coins, total):
     """
     if total < 1:
         return 0
-    
+
     dp = [float('inf')] * (total + 1)
     dp[0] = 0
-    
+
     for i in range(1, total + 1):
         for coin in coins:
             if coin <= i:
