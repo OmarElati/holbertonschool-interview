2b299910c463d5b1471c92915f707ae8388377b5
OmarElati <5796@holbertonstudents.com>
2024-07-12 17:10:55 +0200

    Change comes from within

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -7,7 +7,7 @@ def makeChange(coins, total):
     Given a pile of coins of different values, determine the fewest number
     of coins needed to meet a given amount total.
     """
-    if total < 1:
+    if total < 0:
         return 0
     coins.sort()
     coins.reverse()
